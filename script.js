4ca8e3026ca2cce2c66fc582789e718afb7e62c9
Andrea <amm6798@psu.edu>
2019-06-10 10:28:01 -0400

    drawTheClock!

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ document.getElementById('rickRoll').onclick = function () {
     var ctx = canvas.getContext("2d");
     var radius = canvas.height / 2;
     ctx.translate(radius, radius);
-    radius = radius * 0.70
+    radius = radius * 0.70;
+    drawClock();
+
     setInterval(drawClock, 1000);
 
     function drawClock() {
